bc93122ce2e7764f16a48556378d0882cd6ead04
Rana-Siroosian <rana.siroosian@gmail.com>
2020-04-13 15:29:47 -0400

    added some functionality to the button

--- app/src/main/java/learnprogramming/academy/MainActivity.java
+++ app/src/main/java/learnprogramming/academy/MainActivity.java
@@ -1,6 +1,12 @@
 package learnprogramming.academy;
+/**
+ *
+ * @author >>RanaSiroosian<<
+ */
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userInput;
     private Button button;
     private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,26 @@ public class MainActivity extends AppCompatActivity {
         userInput = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        //make the textView scroll
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numTimesClicked = numTimesClicked +1;
+                String result = "The button got tapped " + numTimesClicked + " time";
+                   if(numTimesClicked != 1){
+                       result += "s";
+                   }
+                   result = result + "\n";
+                    //append adds a line of text in our view
+                    textView.append(result);
+
+
+
+            }
+        };
+
+        button.setOnClickListener(ourOnClickListener);
     }
 }
